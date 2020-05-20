66195c560a414b78d6d185dd771e53974cf8f649
Steinkellner, Kai <Kai.Steinkellner@yahoo.de>
2020-05-20 20:54:44 +0200

    aDDING header component and links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import {Route, Switch} from 'react-router-dom'
+import Header from "./components/header/header.component"
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import ShopPage from "./pages/shop/shop.component";
 
 function App() {
     return (
         <div>
-            <Switch>
-                <Route exact path='/' component={HomePage}/>
-                <Route path='/shop' component={ShopPage}/>
-            </Switch>
+            <Header/>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route path='/shop' component={ShopPage}/>
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
