41a1525de9d849a7f569b80559f9be5e2f143413
Steinkellner, Kai <Kai.Steinkellner@salt-solutions.de>
2020-05-27 14:54:46 +0200

    cartItems Add with redux quantity

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,7 @@ import {createStore, applyMiddleware} from "redux";
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
-
-
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
