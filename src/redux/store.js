7f85208a4f59710ee99c31a3280f48f63cb5f5e1
Steinkellner, Kai <Kai.Steinkellner@yahoo.de>
2020-05-28 00:57:35 +0200

    add cartselector to cartDropDown and cartIcon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
