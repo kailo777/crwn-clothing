8e485cd43961c3db7a40c90855c50cd0d1d9a222
Steinkellner, Kai <Kai.Steinkellner@yahoo.de>
2020-06-23 02:02:11 +0200

    Aufteilung in Backend Code und Frontend Code für Stripe Integration

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {CustomButtonContainer} from './custom-button.style'
-
-const CustomButton = ({children, ...props}) => (
-    <CustomButtonContainer {...props}>{children} </CustomButtonContainer>
-);
-
-export default CustomButton;
